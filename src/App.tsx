d9e405b356568815049b7215cf0a332fe96c28c2
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2022-10-13 23:30:44 +0900

    feat(component): accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
 import { Popover, PopoverTrigger, PopoverContent } from "./components/Popover";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionContent,
+  AccordionTrigger,
+  AccordionHeader,
+} from "./components/Accordion/Accordion";
 
 const App = () => {
   return (
     <div
       style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "40px",
         padding: "16px",
         backgroundColor: "#f5f5f4",
         minHeight: "100vh",
       }}
     >
-      <Popover>
-        <PopoverTrigger>trigger</PopoverTrigger>
-        <PopoverContent>content</PopoverContent>
-      </Popover>
+      <div>
+        <h2>Popover</h2>
+        <Popover>
+          <PopoverTrigger>trigger</PopoverTrigger>
+          <PopoverContent>content</PopoverContent>
+        </Popover>
+      </div>
+
+      <div>
+        <h2>Accordion (CSS modules)</h2>
+        <Accordion type="single">
+          <AccordionItem value="item-1">
+            <AccordionHeader>
+              <AccordionTrigger>title-1</AccordionTrigger>
+            </AccordionHeader>
+            <AccordionContent>content-2</AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="item-2">
+            <AccordionHeader>
+              <AccordionTrigger>title-2</AccordionTrigger>
+            </AccordionHeader>
+            <AccordionContent>content-2</AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 };
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,22 @@
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { withClassName } from "../../utils/withClassName";
+import styles from "./Accordion.module.css";
+
+export const Accordion = withClassName(AccordionPrimitive.Root, styles.route);
+
+export const AccordionItem = withClassName(
+  AccordionPrimitive.Item,
+  styles.item
+);
+export const AccordionHeader = withClassName(
+  AccordionPrimitive.Header,
+  styles.header
+);
+export const AccordionContent = withClassName(
+  AccordionPrimitive.Content,
+  styles.content
+);
+export const AccordionTrigger = withClassName(
+  AccordionPrimitive.Trigger,
+  styles.trigger
+);
